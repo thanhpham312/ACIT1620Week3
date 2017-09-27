2507022ba0f465491d919c28a37f618c2256de95
thanhpham312 <tpham74@my.bcit.ca>
2017-09-26 20:22:41 -0700

    Fist commit.

---
+++ myjs.js
@@ -0,0 +1,23 @@
+document.getElementById("open").addEventListener("click", function() {
+    document.getElementById("controlPanel").style.left = "0";
+});
+
+document.getElementById("open2").addEventListener("click", function() {
+    document.getElementById("controlPanel").style.left = "-160px";
+});
+
+document.getElementById("bg1").addEventListener("click", function() {
+    document.getElementById("backgroundImage").style.backgroundImage = "url(img/bg1.png)";
+});
+document.getElementById("bg2").addEventListener("click", function() {
+    document.getElementById("backgroundImage").style.backgroundImage = "url(img/bg2.png)";
+});
+document.getElementById("bg3").addEventListener("click", function() {
+    document.getElementById("backgroundImage").style.backgroundImage = "url(img/bg3.png)";
+});
+document.getElementById("bg4").addEventListener("click", function() {
+    document.getElementById("backgroundImage").style.backgroundImage = "url(img/bg4.png)";
+});
+document.getElementById("bg5").addEventListener("click", function() {
+    document.getElementById("backgroundImage").style.backgroundImage = "url(img/bg5.png)";
+});
